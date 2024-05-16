5355e2a6a9470ebaf4575d9e57a62483da94d490
Anthony Ngan <anthonyngan74@gmail.com>
2024-05-17 08:32:26 +1200

    configured in-memory db, fixed build errors

--- software-development/backend/Program.cs
+++ software-development/backend/Program.cs
@@ -3,14 +3,22 @@ using Models;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<StudentContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentContext") ?? throw new InvalidOperationException("Connection string 'StudentContext' not found.")));
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<Models.StudentContext>(options =>
-    options.UseInMemoryDatabase("Student"));
+
+// Configure DbContext before building the app
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<StudentContext>(options =>
+        options.UseInMemoryDatabase("Student"));
+}
+else
+{
+    builder.Services.AddDbContext<StudentContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("StudentContext") ?? throw new InvalidOperationException("Connection string 'StudentContext' not found.")));
+}
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
